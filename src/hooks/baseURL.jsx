10fbf614b09a0f381217c9088c5ad8c90c3e69e4
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-08-01 13:24:58 +0630

    test sp

--- src/hooks/baseURL.jsx
+++ src/hooks/baseURL.jsx
@@ -1,3 +1,4 @@
-const BASE_URL = 'https://www.spidermanslotgame.online/api';
+//const BASE_URL = 'https://www.spidermanslotgame.online/api';
+const BASE_URL = 'https://www.allinonetestcase.online/api';
 
 export default BASE_URL;
